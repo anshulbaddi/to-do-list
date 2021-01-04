54b65f3c85417c6994e2d1badddcdc483d2cdb8c
anshulbaddi <69763264+anshulbaddi@users.noreply.github.com>
2021-01-03 17:37:49 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,30 @@
+var listOfActivities = [];
+var input = document.getElementById("input");
+var toDoList = document.getElementById("todolist");
+document.getElementById("button").onclick = click;
+
+function click() {
+    listOfActivities.push(input.value);
+    console.log(listOfActivities);
+    input.value = "";
+    showList();
+
+}
+
+function showList() {
+    toDoList.innerHTML = "";
+    listOfActivities.forEach(function (current_value, index) {
+        toDoList.innerHTML += ("<li>" + current_value + "<a onclick='editItem(" + index + ")'> edit </a>" + "<a onclick='deleteItem(" + index + ")'>&times |</a><l/i>")
+    })
+}
+
+function editItem(index) {
+    var newValue = promt("please insert your value");
+    listOfActivities.splice(index, 1, newValue);
+    showList();
+}
+
+function deleteItem(index) {
+    listOfActivities.splice(index, 1);
+    showList();
+}
